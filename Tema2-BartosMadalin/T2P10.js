d286d101bc26fd01a37c499cc1725fe0d2523b17
dianasecrieru <secrieru_diana@yahoo.com>
2017-03-29 23:51:44 +0300

    Tema2 - Bartos Madalin

---
+++ Tema2-BartosMadalin/T2P10.js
@@ -0,0 +1,4 @@
+var arr = [0, "text", 9, undefined, "undefined", -5, null, "null"];
+
+for(var i=0;i<arr.length;i++)
+    console.log(arr[i] + " is a " + typeof(arr[i]));
---
+++ Tema2-BartosMadalin/T2P4.js
@@ -0,0 +1,11 @@
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1; 
+var year = today.getFullYear();
+
+if(day<10)
+    day="0"+day;
+if(month<10)
+    month="0"+month;
+    
+console.log(day+"-"+month+"-"+year);
