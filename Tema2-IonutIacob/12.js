332ca64d06565056434b80f9238bf428babb9613
Ionut Robert <ionut.iacob17@gmail.com>
2017-03-29 20:18:00 +0300

    Improved 12.js

--- Tema2-IonutIacob/12.js
+++ Tema2-IonutIacob/12.js
@@ -1,29 +1,26 @@
 //12. Afisati in consola in mod crescator valorile din obiectul urmator { first:13, second:6, thrid:0, tenth:34, last:-2}
+
+function sortV(array){
+	var isModified = true;
+	while (isModified){
+		isModified = false;
+		for (var index=0;index<array.length-1;index+=1){
+			if (array[index]>array[index+1]){
+				temp = array[index]
+				array[index] = array[index+1]
+				array[index+1] = temp
+				isModified = true;
+			}
+		}
+	}
+	return array;
+}
+
 var object = { first:13, second:6, thrid:0, tenth:34, last:-2}
+var arrValues = []
 
-function getLength(param){
-	count = 0
-	for (item in param){
-		count += 1
-	}
-	return count
+for (key in object){
+	arrValues.push(object[key]);
 }
+sortV(arrValues).map(function(value){console.log(value)});
 
-while (getLength(object)){
-	var min = {
-		value: undefined,
-		key: undefined
-	}
-	for (var key in object){
-		if (min.value===undefined){
-			min.value = object[key]
-			min.key = key
-		}
-		if (object[key]<min.value){
-			min.value = object[key]
-			min.key = key
-		}
-	};
-	console.log(min.value);
-	delete object[min.key];
-}
